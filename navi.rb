e4dda7fe3a873d2a24bcacc61096b4f6c82ade79
Denis Isidoro <denis_isidoro@live.com>
2019-09-23 19:36:06 -0300

    Revert "navi 0.8.1"
    This reverts commit c2b81dd408d9091ad23e041e4fbd436ff9cb96f2.

--- navi.rb
+++ navi.rb
@@ -1,8 +1,8 @@
 class Navi < Formula
   desc "An interactive cheatsheet tool for the command-line"
   homepage "https://github.com/denisidoro/navi"  
-  url "https://github.com/denisidoro/navi/archive/v0.9.0.tar.gz"
-  sha256 "70acda1f146a9436c5865f33d8a47d4394991812faf2730e8e3036315cc3762c"
+  url "https://github.com/denisidoro/navi/archive/v0.8.0.tar.gz"
+  sha256 "6e4a09caf52f519096e18364cefed55e89075b5fb1b9f5cdf0f09fa4a4e5d284"
 
   depends_on "fzf" => :build
 
