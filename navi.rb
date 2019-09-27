38d689ace755b5005567042f39edf69d7c79c696
Denis Isidoro <denisidoro@users.noreply.github.com>
2019-09-27 16:54:06 -0300

    navi 0.10.1

--- navi.rb
+++ navi.rb
@@ -1,8 +1,8 @@
 class Navi < Formula
   desc "An interactive cheatsheet tool for the command-line"
   homepage "https://github.com/denisidoro/navi"  
-  url "https://github.com/denisidoro/navi/archive/v0.10.0.tar.gz"
-  sha256 "a9f901eb1bb896f3931a13ebb4b63d71ee538da17f2837d069526192934c21e4"
+  url "https://github.com/denisidoro/navi/archive/v0.10.1.tar.gz"
+  sha256 "c6ac7c5dd9a60ad2f2cd7d42bb271ede72dded92b9f0a4aabb39535f8e6a594d"
 
   depends_on "fzf" => :build
 
