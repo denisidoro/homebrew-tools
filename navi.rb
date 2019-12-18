3732b9925ed3db64aedf06a478100d320a77aa1a
Denis Isidoro <denisidoro@users.noreply.github.com>
2019-12-18 13:19:27 -0300

    Attempt to link formula

--- navi.rb
+++ navi.rb
@@ -1,13 +1,9 @@
 class Navi < Formula
   desc "Interactive cheatsheet tool for the command-line"
   homepage "https://github.com/denisidoro/navi"
-  sha256 :no_check
-  head "https://github.com/denisidoro/navi.git", :branch => "master"
-
-  depends_on "fzf"
+  
+  depends_on "navi"
 
   def install
-    libexec.install Dir["*"]
-    bin.write_exec_script (libexec/"navi")
   end
 end
