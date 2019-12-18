432c4ac4f65c480aab32b602faa784db55e364c9
Denis Isidoro <denisidoro@users.noreply.github.com>
2019-12-18 13:23:59 -0300

    Attempt 2

--- navi.rb
+++ navi.rb
@@ -1,9 +1,12 @@
 class Navi < Formula
-  desc "Interactive cheatsheet tool for the command-line"
   homepage "https://github.com/denisidoro/navi"
-  
+  sha256 :no_check
+  head "https://github.com/denisidoro/navi.git", :branch => "master"
+
   depends_on "navi"
 
   def install
+    libexec.install Dir["*"]
+    bin.write_exec_script (libexec/"navi")
   end
 end
