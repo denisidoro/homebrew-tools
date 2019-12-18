137797455a832bd0a7c9438d9046474a3dede185
Denis Isidoro <denisidoro@users.noreply.github.com>
2019-12-18 13:27:13 -0300

    Attempt 3

--- navi.rb
+++ navi.rb
@@ -1,12 +1,14 @@
 class Navi < Formula
+  desc "Interactive cheatsheet tool for the command-line"
   homepage "https://github.com/denisidoro/navi"
-  sha256 :no_check
-  head "https://github.com/denisidoro/navi.git", :branch => "master"
+  url "https://github.com/denisidoro/navi/archive/v0.15.3.tar.gz"
+  sha256 "4b03693751a45ee078f62749df857f24fcbae7fe7b1c8a746bd11a79900a7e44"
 
-  depends_on "navi"
+  depends_on "fzf"
 
   def install
-    libexec.install Dir["*"]
-    bin.write_exec_script (libexec/"navi")
+    #libexec.install Dir["*"]
+    #bin.write_exec_script(libexec/"navi")
+    shell_output("brew install navi")
   end
 end
