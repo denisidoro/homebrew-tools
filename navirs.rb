549ef9dd413ed660158bea8dbedfef17ee36150f
Denis Isidoro <denis_isidoro@live.com>
2020-02-26 22:28:58 -0300

    navirs 2.0.5

--- navirs.rb
+++ navirs.rb
@@ -1,14 +1,14 @@
 class Navirs < Formula
-  version '2.0.4'
+  version '2.0.5'
   desc "An interactive cheatsheet tool for the command-line."
   homepage "https://github.com/denisidoro/navi"
 
   if OS.mac?
       url "https://github.com/denisidoro/navi/releases/download/v#{version}/navi-macos-amd64.tar.gz"
-      sha256 "19e3acde66d30a9d936932aef2e8c371718cec230fc3783c49dbc661e56d451d"
+      sha256 "577aed8017d431595089da16a0424c7fb2b001c799d9e5aa28700d0202ec47fa"
   elsif OS.linux?
       url "https://github.com/denisidoro/navi/releases/download/v#{version}/navi-linux-amd64.tar.gz"
-      sha256 "854bda3c6a26edc41351b58976a98417cd75bec4f91262de0e15ccc637802a7b"
+      sha256 "537e72771b970bf21828897abb51ece9af4a7d419bd916c55a9d601a465ccfd1"
   end
 
   conflicts_with "navi", :because => "both install `navi` binaries"
