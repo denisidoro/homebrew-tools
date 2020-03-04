bfe8838ad9d09788456d28f8190567a9b17990c9
Denis Isidoro <denisidoro@users.noreply.github.com>
2020-03-04 18:12:41 -0300

    navirs 2.0.0

--- navirs.rb
+++ navirs.rb
@@ -1,26 +1,24 @@
 class Navirs < Formula
-  version '2.0.5'
-  desc "An interactive cheatsheet tool for the command-line."
+  version '2.0.0'
+  desc "Interactive cheatsheet tool for the command-line."
   homepage "https://github.com/denisidoro/navi"
 
   if OS.mac?
-      url "https://github.com/denisidoro/navi/releases/download/v#{version}/navi-macos-amd64.tar.gz"
-      sha256 "577aed8017d431595089da16a0424c7fb2b001c799d9e5aa28700d0202ec47fa"
+      url "https://github.com/denisidoro/navi/releases/download/v#{version}/navi-x86_64-osx.tar.gz"
+      sha256 "fbaa842936a1d82e48f4417d1571f8e0f58d50dfa76d20b50283fbd9d62efc97"
   elsif OS.linux?
-      url "https://github.com/denisidoro/navi/releases/download/v#{version}/navi-linux-amd64.tar.gz"
-      sha256 "537e72771b970bf21828897abb51ece9af4a7d419bd916c55a9d601a465ccfd1"
+      url "https://github.com/denisidoro/navi/releases/download/v#{version}/navi-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "0d1b7b83ed12177e1f506f8e4bd9ac7f8e113bc3f977416ac8703d912fb13593"
   end
 
-  conflicts_with "navi", :because => "both install `navi` binaries"
   depends_on "fzf"
 
   def install
     libexec.install Dir["*"]
     bin.write_exec_script (libexec/"navi")
-    # man1.install "doc/rg.1"
-
-    # bash_completion.install "complete/rg.bash"
-    # fish_completion.install "complete/rg.fish"
-    # zsh_completion.install "complete/_rg"
   end
+  
+  test do
+    assert_equal "navi " + version, shell_output("#{bin}/navi --version")
+  end  
 end
