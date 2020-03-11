56d254d680afb142f3cde42cb2355b374bf9cd79
Denis Isidoro <denis_isidoro@live.com>
2020-03-11 12:39:46 -0300

    navi 2.0.3

--- navi.rb
+++ navi.rb
@@ -1,13 +1,27 @@
 class Navi < Formula
-  desc "Interactive cheatsheet tool for the command-line"
+  version '2.0.3'
+  desc "Interactive cheatsheet tool for the command-line."
   homepage "https://github.com/denisidoro/navi"
-  sha256 :no_check
-  head "https://github.com/denisidoro/navi.git", :branch => "master"
+
+  if OS.mac?
+      url "https://github.com/denisidoro/navi/releases/download/v#{version}/navi-x86_64-osx.tar.gz"
+      sha256 "71061fcbb9742552a6d1d563b3e6dd25101b89d4952862cd2ab9eae5a4a9e79c"
+  elsif OS.linux?
+      url "https://github.com/denisidoro/navi/releases/download/v#{version}/navi-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "c5f5301d053396b6cbb068e5b062d331ac263ce7abe1dc7f354f15ae97382d0a"
+  end
 
   depends_on "fzf"
 
   def install
-    libexec.install Dir["*"]
-    bin.write_exec_script (libexec/"navi")
+    (libexec/"cheats").install Dir["cheats/*"]
+    (libexec/"shell").install Dir["shell/*"]
+    bin.install "navi"
+  end
+
+  test do
+    assert_match "navi " + version, shell_output("#{bin}/navi --version")
+    (testpath/"cheats/test.cheat").write "% test\n\n# foo\necho bar\n\n# lorem\necho ipsum\n"
+    assert_match "bar", shell_output("export RUST_BACKTRACE=1; #{bin}/navi --path #{testpath}/cheats best foo")
   end
 end
