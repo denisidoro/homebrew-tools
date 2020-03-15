4f5bfccf09209753fa42ebf2f78ca09cb2c7415a
Denis Isidoro <denisidoro@users.noreply.github.com>
2020-03-15 14:16:02 -0300

    navi 2.1.0

--- navi.rb
+++ navi.rb
@@ -1,21 +1,19 @@
 class Navi < Formula
-  version '2.0.11'
+  version '2.1.0'
   desc "Interactive cheatsheet tool for the command-line."
   homepage "https://github.com/denisidoro/navi"
 
   if OS.mac?
       url "https://github.com/denisidoro/navi/releases/download/v#{version}/navi-x86_64-osx.tar.gz"
-      sha256 "3c6e9ca5461d49efeed9662f326a2868da20d6e0b3fe639dc55a22230a96b07e"
+      sha256 "804c04d383cd8a54ae30af6b7bbf402658f486c321c6b8f2b47e4dff6a2271cd"
   elsif OS.linux?
       url "https://github.com/denisidoro/navi/releases/download/v#{version}/navi-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "27df4c57f6ba2f12bfb1a1fe06cdb272990d11dc74a4e8de79d23a62fb884509"
+      sha256 "e36175a9b2d3cff0c6490a6e4eca4cf3f8f21b94965526f00066500aefcddc3b"
   end
 
   depends_on "fzf"
 
   def install
-    (libexec/"cheats").install Dir["cheats/*"]
-    (libexec/"shell").install Dir["shell/*"]
     bin.install "navi"
   end
 
