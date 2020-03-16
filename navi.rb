c0c52384413123c78d30f8e03d8c419ca048bddb
Denis Isidoro <denisidoro@users.noreply.github.com>
2020-03-15 22:29:31 -0300

    navi 2.1.1

--- navi.rb
+++ navi.rb
@@ -1,14 +1,14 @@
 class Navi < Formula
-  version '2.1.0'
+  version '2.1.1'
   desc "Interactive cheatsheet tool for the command-line."
   homepage "https://github.com/denisidoro/navi"
 
   if OS.mac?
       url "https://github.com/denisidoro/navi/releases/download/v#{version}/navi-x86_64-osx.tar.gz"
-      sha256 "804c04d383cd8a54ae30af6b7bbf402658f486c321c6b8f2b47e4dff6a2271cd"
+      sha256 "0cfef4d3562e42f8fa5354cdb86a1fb041b2d88a17fcee91961d912a2c612165"
   elsif OS.linux?
       url "https://github.com/denisidoro/navi/releases/download/v#{version}/navi-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "e36175a9b2d3cff0c6490a6e4eca4cf3f8f21b94965526f00066500aefcddc3b"
+      sha256 "1f14a55fdef7a43e51592b6746a263bfc6fd5379162569a34f6c8fdb9c97b7a0"
   end
 
   depends_on "fzf"
