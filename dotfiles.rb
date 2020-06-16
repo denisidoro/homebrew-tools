6183e67405834725eac978827972dd90a91d5776
Denis Isidoro <denisidoro@users.noreply.github.com>
2020-06-16 17:29:56 -0300

    dot self install

--- dotfiles.rb
+++ dotfiles.rb
@@ -4,10 +4,9 @@ class Dotfiles < Formula
   url "https://github.com/denisidoro/dotfiles/archive/v2019.12.09.tar.gz"
   sha256 "bac7e5a3285c7eba515c81a57ef74975153faa25e5ba659a967a54859b78d834"
 
-  depends_on "fzf"
-
   def install
     libexec.install Dir["*"]
     bin.write_exec_script(libexec/"bin/dot")
+    shell_output("DOTFILES="libexec"; "libexec"/bin/dot self install")
   end
 end
