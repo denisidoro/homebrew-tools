978c72c4e00f89ccc79a1384e0f81d31c187f716
Denis Isidoro <denisidoro@users.noreply.github.com>
2020-08-04 18:02:09 -0300

    Update dotfiles.rb

--- dotfiles.rb
+++ dotfiles.rb
@@ -1,8 +1,8 @@
 class Dotfiles < Formula
   desc "Personal dotfiles"
   homepage "https://github.com/denisidoro/dotfiles"
-  url "https://github.com/denisidoro/dotfiles/archive/v2019.12.09.tar.gz"
-  sha256 "5f7d4bb5b53583f6a333a3eb7939e22f6b070cd298f41acd9ba1404abec669c9"
+  url "https://github.com/denisidoro/dotfiles/archive/v2020.08.03.tar.gz"
+  sha256 "c98d8dfcc1b39110496b1005aa509178aef8033cf9f45db03e2bf4de18d76fb2"
 
   def install
     libexec.install Dir["*"]
