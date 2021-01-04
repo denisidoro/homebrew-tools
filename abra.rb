10bddec1cd9e89126f82826c8d9dacbc2de28e85
Denis Isidoro <denis.isidoro@uber.com>
2021-01-04 17:23:56 -0300

    Jan-04 17h23: abra.rb

--- abra.rb
+++ abra.rb
@@ -5,10 +5,10 @@ class Abra < Formula
 
   if OS.mac?
       url "https://github.com/denisidoro/abra/releases/download/v#{version}/abra-v#{version}-x86_64-osx.tar.gz"
-      sha256 "5f4e974a0d2919737028f63320750867ed8b10b53da2fe3b63890e3bce7609c3"
+      sha256 "c92572c3be8218d62e117b85c37cc235b161c25a1f0d0af81dde6e7c5b195695"
   elsif OS.linux?
       url "https://github.com/denisidoro/abra/releases/download/v#{version}/abra-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "c92572c3be8218d62e117b85c37cc235b161c25a1f0d0af81dde6e7c5b195695"
+      sha256 "5f4e974a0d2919737028f63320750867ed8b10b53da2fe3b63890e3bce7609c3"
   end
 
   def install
